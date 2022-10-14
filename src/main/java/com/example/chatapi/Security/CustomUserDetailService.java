b24acc8758abfb13d87db01e8bb906847657dd60
42bahn <bbu0704@gmail.com>
2022-10-14 12:32:46 +0900

    Save Chatting Log in Chat rooms

--- src/main/java/com/example/chatapi/Security/CustomUserDetailService.java
+++ src/main/java/com/example/chatapi/Security/CustomUserDetailService.java
@@ -10,11 +10,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Component("userDetailsService")
+//@Component("userDetailsService")
+@Service
 public class CustomUserDetailService implements UserDetailsService {
 
     private final Logger log = LoggerFactory.getLogger(CustomUserDetailService.class);
