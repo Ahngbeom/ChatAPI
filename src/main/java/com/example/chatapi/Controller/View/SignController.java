493f305d6fcc43d1e49a25855b872b9b9c298da2
42bahn <bbu0704@gmail.com>
2022-11-02 14:23:23 +0900

    I will be implementing a login failure handler.

--- src/main/java/com/example/chatapi/Controller/View/SignController.java
+++ src/main/java/com/example/chatapi/Controller/View/SignController.java
@@ -1,9 +1,9 @@
 package com.example.chatapi.Controller.View;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
 
@@ -12,17 +12,25 @@ import java.security.Principal;
 public class SignController {
 
     @GetMapping("/login")
-    public String login(Principal principal, String logout, String error) {
-//        log.info(logout);
-//        log.info(error);
-        if (principal != null)
-            return "redirect:/mbti";
-        return "pages/user/login";
+    public ModelAndView login(ModelAndView mv, Principal principal, String logout, String error) {
+        log.info("logout: " + logout);
+        log.info("error: " + error);
+//        if (principal != null)
+//            mv.setViewName("redirect:/");
+        if (logout != null) {
+            mv.addObject("server_message", "로그아웃되었습니다.");
+        }
+        if (error != null) {
+            mv.addObject("server_message", "다시 로그인해주세요.");
+        }
+        mv.setViewName("pages/user/login");
+        return mv;
     }
 
     @GetMapping("/signup")
-    public String signup() {
-        return "pages/user/signup";
+    public ModelAndView signup(ModelAndView mv) {
+        mv.setViewName("pages/user/signup");
+        return mv;
     }
 
 }
